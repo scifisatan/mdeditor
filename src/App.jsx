89377553a3f50418c74711598741e444d1a8155b
scifisatan <brawlershere@gmail.com>
2023-07-13 19:47:07 +0545

    added some texts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
 import { useState } from 'react';
 import ReactMarkdown  from 'react-markdown';
-export default function App() { 
 
-    const [markdown, setMarkdown] = useState('# sup');
+export default function App() { 
+  
+  const [markdown, setMarkdown] = useState('# This is heading 1\n## This is heading 2\n### This is heading 3\n\nThis is *italics* , `coded`, **bold** texts.\n\n- this is bullet points\n- another point');
 
   return (
     <div className="app">
